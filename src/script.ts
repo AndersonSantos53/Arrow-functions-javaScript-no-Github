13cba811f78b8186a77e7e315f3a379162962c99
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-09-27 15:16:44 -0300

    Update script.ts

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,9 @@
 const imc = (peso: number, altura: number) => { return peso / (Math.pow(altura, 2));
 }
 
-const peso = Number(prompt(`Digite o peso: `));
+const peso = Number(prompt(`Digite o peso (EX: 70.5): `));
 
-const altura = Number(prompt(`Digite sua altura: `));
+const altura = Number(prompt(`Digite sua altura (EX: 1.71): `));
 
 const resultadoIMC = imc(peso, altura)
 console.log(`Seu IMC é: ${resultadoIMC}`)

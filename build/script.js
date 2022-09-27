dea0ceee6e4d5a47bf6367040116c5cc996b8d3a
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-09-27 15:17:57 -0300

    Update script.js

--- build/script.js
+++ build/script.js
@@ -1,7 +1,7 @@
 var imc = function (peso, altura) {
     return peso / (Math.pow(altura, 2));
 };
-var peso = Number(prompt("Digite o peso: "));
-var altura = Number(prompt("Digite sua altura: "));
+var peso = Number(prompt("Digite o peso (EX: 70.5): "));
+var altura = Number(prompt("Digite sua altura (EX: 1.71): "));
 var resultadoIMC = imc(peso, altura);
 console.log("Seu IMC \u00E9: ".concat(resultadoIMC));
